818ff5ecfbe9e781b3c7c9a5d46d1c38cd486529
Fuzi8922 <voyage1969to70@gmail.com>
2022-03-28 12:03:26 +0900

    メッセージ機能の実装、ビュー画面の修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: "rooms#index"
 
   resources :users
-  resources :rooms
+  resources :rooms,only: [:new,:create] do
+    resources :messages,only: [:index,:create]
+  end
 
 end
